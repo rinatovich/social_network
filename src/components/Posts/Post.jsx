653f7ea59b812961858138ba826aefafff1d32f9
rinatovich <mvrinatovicso@gmail.com>
2021-01-26 10:22:45 +0500

    26.01.2020 Props

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './Posts.module.css';
-const Post = ()=>{
+const Post = (props)=>{
     return(
         <div className={style.post}>
             <div className={style.avatar}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHnpNcjCtkByOd98wZFcme0ff7TQXi68VCLQ&usqp=CAU" alt="user_avatar"/></div>
-            <div className={style.text}>Hey, how are you</div>
+            <div className={style.text}>{props.text}</div>
+            <div className={style.likes}><i class="fas fa-thumbs-up"></i><span className={style.likeCounter}>{props.likesCount}</span></div>
         </div>
     )
 }
--- src/components/Posts/Postsarea.jsx
+++ src/components/Posts/Postsarea.jsx
@@ -4,8 +4,8 @@ import style from './Posts.module.css';
 const Postsarea = ()=>{
     return (
         <div className={style.postsarea}>
-            <Post />
-            <Post />
+            <Post text = 'Hello world' likesCount = '30'/>
+            <Post text = 'Its my first post' likesCount = '40'/>
         </div>
     )
 }
